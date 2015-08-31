08c49ac0a46f5f872bb46629825e97e7d07f8518
ajisudarmawan <ajinet2002@yahoo.com>
2015-09-01 06:51:22 +0700

    Create DemoAritmatika.java

---
+++ DemoAritmatika.java
@@ -0,0 +1,43 @@
+/**
+ *
+ * @author aji sudarmawan
+ */
+public class DemoAritmatika {
+    public static void main(String[] args){
+    //contoh angka
+        int m = 27;
+        int p = 33;
+        double x = 26.485;
+        double y = 9.52;
+    System.out.println("Nilai Variabel...");
+    System.out.println("m = " + m);
+    System.out.println("p = " + p);
+    System.out.println("x = " + x);
+    System.out.println("y = " + y); 
+    //penjumlahan
+    System.out.println("Adding...");
+    System.out.println("m + p = " + (m + p));
+    System.out.println("x + y = " + (x + y));
+    //pengurangan
+    System.out.println("Subtracting...");
+    System.out.println("m - p = " + (m - p));
+    System.out.println("x - y = " + (x - y));
+    //perkalian angka
+    System.out.println("Multiplying...");
+    System.out.println("m * p = " + (m * p));
+    System.out.println("x * y = " + (x * y));
+    //pembagian
+    System.out.println("Dividing...");
+    System.out.println("m / p = " + (m / p ));
+    System.out.println("x / y = " + (x / y ));
+    //menghitung hasil modulus dari pembagian
+    System.out.println("Modulus ...");
+    System.out.println("m % p = " + (m % p));
+    System.out.println("x % y = " + (x % y));
+    //tipe penggabungan
+    System.out.println("jika menggunakan campuran tipe data ...");
+    System.out.println("j + y = " + (p + y));
+    System.out.println("m * x = " + (m * x));
+    
+    }
+}
