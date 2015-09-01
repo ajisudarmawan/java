4be137d47bbecd2378f50fab8d786925b17b6c03
ajisudarmawan <ajinet2002@yahoo.com>
2015-09-01 21:39:47 +0700

    Create OperatorIncrementDanDecrement.java

---
+++ OperatorIncrementDanDecrement.java
@@ -0,0 +1,58 @@
+/**
+ *
+ * @author aji sudarmawan
+ */
+public class OperatorIncrementDanDecrement {
+     public static void main (String args[]) {
+
+         // Contoh Increment
+         
+          int a = 1;
+
+          int b = 2;
+
+          int c;
+
+          int d;
+
+          c = ++b;
+
+          d = a++;
+
+          c++;
+
+          System.out.println("a = " +  a);
+
+          System.out.println("b= " +  b);
+
+          System.out.println("c = " + c);
+
+          System.out.println("d = " + d);
+          
+          
+          // Contoh Decrement
+          
+          int e = 5;
+
+          int f = 2;
+
+          int g;
+
+          int h;
+
+          g = --f;
+
+          h = e--;
+
+          g--;
+
+          System.out.println("e = " +  e);
+
+          System.out.println("f= " +  f);
+
+          System.out.println("g = " + g);
+
+          System.out.println("h = " + h);
+          
+     }   
+}
