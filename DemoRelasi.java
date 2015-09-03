27179204d1243235f14762fd682d37ddf1f0eb10
ajisudarmawan <ajinet2002@yahoo.com>
2015-09-03 18:55:48 +0700

    Create DemoRelasi.java

---
+++ DemoRelasi.java
@@ -0,0 +1,45 @@
+/**
+ *
+ * @author aji sudarmawan
+ */
+public class DemoRelasi {
+ public static void main(String[] args) {
+//beberapa nilai
+    int a = 58;
+    int b = 44;
+    int c = 44;
+    System.out.println("Nilai variabel...");
+    System.out.println(" a = " + a);
+    System.out.println(" b = " + b);
+    System.out.println(" c = " + c);
+
+//lebih besar dari
+    System.out.println("Lebih besar dari");
+    System.out.println("a > b = " + (a > b)); 
+    System.out.println("b > a = " + (b > a)); 
+    System.out.println(" c > b = " + (c > b)); 
+//lebih besar atau sama dengan
+    System.out.println("Lebih besar dari atau sama dengan...");
+    System.out.println("a >= b = " + (a >= b)); 
+    System.out.println("b >= a = " + (b >= a)); 
+    System.out.println("c >= b = " + (c >= b)); 
+//lebih kecil dari
+    System.out.println("Lebih kecil dari ");
+    System.out.println(" a < b = " + (a < b)); 
+    System.out.println(" b < a = " + (b < a)); 
+    System.out.println(" c < b = " + (c < b)); 
+//lebih kecil atau sama dengan
+    System.out.println("Lebih kecil dari atau sama dengan");
+    System.out.println("a <= b = " + (a <= b)); 
+    System.out.println("b <= a = " + (b <= a)); 
+    System.out.println("c <= b = " + (c <= b)); 
+//sama dengan
+    System.out.println("Sama dengan...");
+    System.out.println(" a == b = " + (a == b)); 
+    System.out.println(" c == b = " + (c == b)); 
+//tidak sama dengan
+    System.out.println("Tidak sama dengan...");
+    System.out.println(" a != b = " + (a != b)); 
+    System.out.println(" c != b = " + (c != b)); 
+    }   
+}
