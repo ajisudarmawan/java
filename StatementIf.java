cb655386e187d013faae522f04af4cb3d824338a
ajisudarmawan <ajinet2002@yahoo.com>
2015-09-06 20:12:47 +0700

    Create StatementIf.java

---
+++ StatementIf.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author aji sudarmawan
+ */
+public class StatementIf {
+public static void main(String[] args){
+    int grade = 78;
+    if( grade > 70 ){
+    System.out.println("Selamat ya!");
+    System.out.println("Anda Lolos!");
+    }
+}    
+}
