1fe8f376cf4f77f38e54ca9607cfc96fea920bb5
ajisudarmawan <ajinet2002@yahoo.com>
2015-09-07 19:24:20 +0700

    Create StatementIfElse.java

---
+++ StatementIfElse.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author aji sudarmawan
+ */
+public class StatementIfElse {
+public static void main(String[] args){
+    int grade = 68;
+    if( grade > 60 ){
+    System.out.println("Selamat ya ");
+    System.out.println("anda lolos!");
+        }
+    else{
+    System.out.println("maaf anda gagal !!!");
+    }    
+  }    
+}
