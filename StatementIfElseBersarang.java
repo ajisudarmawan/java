b1395b56fae8586178ef6c18c3a02d9c0040dfc2
ajisudarmawan <ajinet2002@yahoo.com>
2015-09-10 19:50:07 +0700

    Create StatementIfElseBersarang.java

---
+++ StatementIfElseBersarang.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author aji sudarmawan
+ */
+public class StatementIfElseBersarang {
+public static void main(String[] args){
+    int x = 40;
+    int y = 70;
+    
+    if ( x == 40 ){
+        if( y == 70 ){
+            System.out.println("X = " + x + " Y = " + y );
+        }
+    }
+}    
+}
