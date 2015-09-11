17b36fd4c6b239196160c6dd24f0d6330c6e0822
ajisudarmawan <ajinet2002@yahoo.com>
2015-09-11 21:27:14 +0700

    Create StatementSwitch.java

---
+++ StatementSwitch.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author aji sudarmawan
+ */
+public class StatementSwitch {
+public static void main( String[] args ){
+    int grade = 1;
+    switch(grade){
+    case 1:
+        System.out.println( "A" );
+        break;
+    case 2:
+        System.out.println("B" );
+        break;
+    case 3:
+        System.out.println("C" );
+        break;
+    default:
+        System.out.println("D");
+    }
+  }    
+}
