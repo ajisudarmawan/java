a18f767aad9661ca3d86e83a30ec6b46cb19bdbc
ajisudarmawan <ajinet2002@yahoo.com>
2015-09-13 20:49:05 +0700

    Create PerulanganWhile.java

---
+++ PerulanganWhile.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author aji sudarmawan
+ */
+public class PerulanganWhile {
+public static void main(String[] args){
+    int i = 4;
+    while ( i < 10 ){
+    System.out.print(i);
+    i++;
+    }
+  }    
+}
