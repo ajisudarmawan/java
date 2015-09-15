3c82c439a501180c6f924857b9690fdd879e7f80
ajisudarmawan <ajinet2002@yahoo.com>
2015-09-15 21:12:30 +0700

    Create PerulanganFor.java

---
+++ PerulanganFor.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author aji sudarmawan
+ */
+public class PerulanganFor {
+public static void main(String[] args){
+     int j;
+     for( j = 0; j < 10; j++ ){
+     System.out.print(j);
+     }
+  }    
+}
