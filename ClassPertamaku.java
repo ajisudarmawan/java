d008f211308ea07dec1bfc44c7400910894ae239
ajisudarmawan <ajinet2002@yahoo.com>
2015-09-23 20:52:28 +0700

    Create ClassPertamaku.java

---
+++ ClassPertamaku.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author aji sudarmawan
+ */
+
+public class ClassPertamaku {
+
+    String pesan="Halo Apa kabar";
+            
+public static void main(String[] args){
+
+ ClassPertamaku objekDibuat = new ClassPertamaku();
+    System.out.println(objekDibuat.pesan);
+}            
+    
+}
