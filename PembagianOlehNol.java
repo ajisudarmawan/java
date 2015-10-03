bf0b1aee5793d9f094922f8014afc894042dcdad
ajisudarmawan <ajinet2002@yahoo.com>
2015-10-03 20:08:45 +0700

    Create PembagianOlehNol.java

---
+++ PembagianOlehNol.java
@@ -0,0 +1,14 @@
+
+/**
+ *
+ * @author aji sudarmawan
+ * 
+ */
+public class PembagianOlehNol {
+    public static void main(String[] args){
+        int nol= 0;
+        int pembagi = 10;
+        
+        int hasil = pembagi/nol;
+    }
+}
